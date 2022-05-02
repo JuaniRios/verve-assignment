d6a0a246dfda4840e1d931881001c58a723d1f8a
juani <juani.rios.99@gmail.com>
2022-05-02 11:24:30 +0200

    route / now displays the md file as html

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
+const fs = require('fs');
+
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const showdown = require('showdown');
+
+showdown.setFlavor('vanilla');
 
 require('dotenv').config();
 
@@ -15,10 +20,15 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+let README_to_html = '';
+const README_to_txt = fs.readFileSync('README.md', 'utf8');
+
+
+const converter = new showdown.Converter();
+README_to_html = converter.makeHtml(README_to_txt);
 app.get('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
-  });
+  res.setHeader('Content-Type', 'text/html');
+  res.send(README_to_html);
 });
 
 app.use('/api/v1', api);
